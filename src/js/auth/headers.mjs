7bda4a370e6162ccb25e677417ab04ce48deb658
Liridona <91525128+barlaek@users.noreply.github.com>
2022-12-03 14:16:46 +0100

    headers

---
+++ src/js/auth/headers.mjs
@@ -0,0 +1,16 @@
+import * as storage from "./storage";
+
+export const headers = (contentType) => {
+  const token = storage.loadToken("token");
+  const headers = {};
+
+  if (contentType) {
+    headers["Content-Type"] = contentType;
+  }
+
+  if (token) {
+    headers.Authorization = `Bearer${token}`;
+  }
+
+  return headers;
+};
