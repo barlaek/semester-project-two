e43bf97285b2f43ee9dbe7ba29e7e0defcbdff73
Liridona <91525128+barlaek@users.noreply.github.com>
2022-12-03 14:06:55 +0100

    loads token

--- src/js/auth/storage.mjs
+++ src/js/auth/storage.mjs
@@ -1,3 +1,23 @@
+/**
+ * Saves an object to window storage
+ * @param {name} key
+ * @param {token} value
+ */
+
 export function saveToken(key, value) {
   localStorage.setItem(key, JSON.stringify(value));
+}
+
+/**
+ * Fetches the token from window storage
+ * @param {key} key
+ * @returns a parsed token
+ */
+
+export function loadToken(key) {
+  try {
+    return localStorage.getItem(JSON.parse(key));
+  } catch {
+    return null;
+  }
 }
