a52613035bf4bcfbc49b4bc593d989c36432ba70
Liridona <91525128+barlaek@users.noreply.github.com>
2022-12-04 16:15:13 +0100

    search params

--- index.mjs
+++ index.mjs
@@ -1,2 +1,3 @@
 import * as auth from "/src/js/auth/index";
 import * as profiles from "/src/js/profiles";
+import * as api from "/src/js/api/index";
--- src/js/api/searchParams.mjs
+++ src/js/api/searchParams.mjs
@@ -3,3 +3,5 @@ import { API_PROFILES } from "./apiPaths.mjs";
 export const queryString = document.location.search;
 export const params = new URLSearchParams(queryString);
 export const name = params.get('name');
+
+console.log(name);
