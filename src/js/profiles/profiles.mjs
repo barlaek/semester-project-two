e1a34f4262c163ef09413a2fa4f200852eb99fef
Liridona <91525128+barlaek@users.noreply.github.com>
2022-12-04 15:15:41 +0100

    debugging path

--- src/js/profiles/profiles.mjs
+++ src/js/profiles/profiles.mjs
@@ -2,7 +2,13 @@ import { API_PROFILES } from "../api/apiPaths.mjs";
 import { headers } from "../auth/headers.mjs";
 
 export async function getProfiles() {
-
+    const response = await fetch(`${API_PROFILES}`, {
+        method: "GET",
+        headers: headers("application/json"),
+    })
+    console.log(response);
+    const json = await response.json();
+    console.log(json);
 }
 
 getProfiles();
