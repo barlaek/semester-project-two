a9cd8ef3a9cb559df8913c96115b5587d0417a13
Liridona <91525128+barlaek@users.noreply.github.com>
2022-12-04 13:17:21 +0100

    debugging registration function

--- src/js/auth/register.mjs
+++ src/js/auth/register.mjs
@@ -1,65 +1,95 @@
 import { API_LOGIN, API_REGISTER } from "../api/apiPaths.mjs";
 import { headers } from "./headers.mjs";
 
-export async function register(name, email, password) {
-  const response = await fetch(`${API_REGISTER}`, {
-    method: "POST",
-    body: JSON.stringify({ name, email, password }),
-    headers: headers("application.json"),
-  });
+// export async function register(name, email, password) {
+//   const response = await fetch(`${API_REGISTER}`, {
+//     method: "POST",
+//     body: JSON.stringify({ name, email, password }),
+//     headers: headers("application.json"),
+//   });
 
-  console.log(response);
+//   console.log(response);
 
-  if (response.ok) {
-    return await response.json();
-  }
+//   if (response.ok) {
+//     return await response.json();
+//   }
 
-  throw new Error(response.statusText);
-}
+//   throw new Error(response.statusText);
+// }
 
-export async function registerListener(event) {
-  event.preventDefault();
-  try {
-    const form = event.target;
-    const data = new FormData(form);
-    const name = data.get("name");
-    const email = data.get("email");
-    const password = data.get("password");
-    register(name, email, password);
-    location.reload();
-  } catch (error) {
-    console.log(error);
-  }
-}
+// export async function registerListener(event) {
+//   event.preventDefault();
+//   try {
+//     const form = event.target;
+//     const data = new FormData(form);
+//     const name = data.get("name");
+//     const email = data.get("email");
+//     const password = data.get("password");
+//     register(name, email, password);
+//     location.reload();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-document
-  .getElementById("registrationForm")
-  .addEventListener("submit", registerListener);
+// document
+//   .getElementById("registrationForm")
+//   .addEventListener("submit", registerListener);
 
 const registeredUser = {
-  name: "bombombadil",
-  email: "bombombadil_ba@stud.noroff.no",
-  password: "1234567890",
+  name: "aassjulebrus",
+  email: "aassjulebrus@stud.noroff.no",
+  password: "assjulebrus69",
 };
 
 console.log(registeredUser);
 
-async function loginUser(email, password) {
-  try {
-    const response = await fetch(`${API_LOGIN}`, {
-      method: "POST",
-      body: JSON.stringify({ email, password }),
-      headers: headers("application.json"),
-    });
+export async function registerUser(user) {
+  const postData = {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(user),
+  };
 
-    if (response.ok) {
-      return await response.json();
-    }
-
-    console.log(response);
-  } catch (error) {
-    console.log(error);
-  }
+  const response = await fetch(`${API_REGISTER}`, postData);
+  console.log(response);
+  const json = await response.json();
+  console.log(json);
 }
 
-loginUser({ email: "bombombadil_ba@stud.noroff.no", password: "1234567890" });
+registerUser();
+
+const loginData = {
+  email: "bombombadil_ba@stud.noroff.no",
+  password: "1234567890",
+};
+
+export async function loginUser(user) {
+  const postData = {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(user),
+  };
+
+  const response = await fetch(`${API_LOGIN}`, postData);
+  console.log(response);
+}
+// async function loginUser(loginData) {
+//   try {
+//     const response = await fetch(`${API_LOGIN}`, {
+//       method: "POST",
+//       body: JSON.stringify(loginData),
+//       headers: headers("application.json"),
+//     });
+
+//     if (response.ok) {
+//       return await response.json();
+//     }
+
+//     console.log(response);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// loginUser(loginData);
