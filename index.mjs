259013df6a1400705bc03c0d5cf351812e5e7082
Liridona <91525128+barlaek@users.noreply.github.com>
2022-12-04 15:10:27 +0100

    profiles pathway upstream

--- index.mjs
+++ index.mjs
@@ -1,1 +1,2 @@
 import * as auth from "/src/js/auth/index";
+import * as profiles from "/src/js/profiles";
---
+++ src/js/profiles/profiles.mjs
@@ -0,0 +1,8 @@
+import { API_PROFILES } from "../api/apiPaths.mjs";
+import { headers } from "../auth/headers.mjs";
+
+export async function getProfiles() {
+
+}
+
+getProfiles();
