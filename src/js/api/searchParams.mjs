6e845acdf3e20117c1c2a2f01db0765ca9b5e994
Bjørn Arne Læknes <91525128+barlaek@users.noreply.github.com>
2022-12-05 15:36:52 +0100

    searchParams saved

--- src/js/api/searchParams.mjs
+++ src/js/api/searchParams.mjs
@@ -2,7 +2,7 @@ import { API_PROFILES } from "./apiPaths.mjs";
 
 export const queryString = document.location.search;
 export const params = new URLSearchParams(queryString);
-export const name = params.get('name');
+export const name = params.get("name");
 export const profileUrl = `${API_PROFILES}/${name}`;
 
 console.log(profileUrl);
