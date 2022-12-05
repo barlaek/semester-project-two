efd5090cc3dfe70089fb6c5462b3794d449f3b04
Bjørn Arne Læknes <91525128+barlaek@users.noreply.github.com>
2022-12-05 15:31:28 +0100

    import listeneres to index.mjs at root

--- index.mjs
+++ index.mjs
@@ -1,3 +1,4 @@
 import * as auth from "/src/js/auth/index";
 import * as profiles from "/src/js/profiles";
 import * as api from "/src/js/api/index";
+import * as listeners from "/src/js/listeners/index";
