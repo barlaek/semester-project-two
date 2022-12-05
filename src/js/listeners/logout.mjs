621d627c819ae90deedf3854ae15719131d40776
Bjørn Arne Læknes <91525128+barlaek@users.noreply.github.com>
2022-12-05 15:35:08 +0100

    logout form

--- src/js/listeners/logout.mjs
+++ src/js/listeners/logout.mjs
@@ -1,8 +1,8 @@
 import { logoutUser } from "../auth/logout.mjs";
 
 export const logout = document
-  .getElementById("logoutBtn")
-  .addEventListener("click", (event) => {
+  .getElementById("logoutForm")
+  .addEventListener("submit", (event) => {
     event.preventDefault();
     logoutUser();
     window.location.href = "/";
