b84642815d6ba7767b7a6c6e23dd5d470498e4fb
Liridona <91525128+barlaek@users.noreply.github.com>
2022-12-06 20:47:56 +0100

    get listings

--- src/js/listings/listings.mjs
+++ src/js/listings/listings.mjs
@@ -1,10 +1,20 @@
 import { API_LISTINGS } from "../api/apiPaths.mjs";
+import { loadToken } from "../storage/storage.mjs";
+import { headers } from "../auth/headers.mjs";
 
 export async function getListings() {
-  const response = await fetch(API_LISTINGS);
+  const getData = {
+    method: "GET",
+    headers: headers("application/json"),
+    body: JSON.stringify(),
+  };
+  const response = await fetch(`${API_LISTINGS}`, getData);
+  console.log(response);
   if (response.ok) {
     return await response.json();
   }
 
   throw new Error(response.statusText);
 }
+
+getListings();
