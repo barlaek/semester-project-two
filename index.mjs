3981b2ad06998821eb69d168c3fcb435eff71ec8
Liridona <91525128+barlaek@users.noreply.github.com>
2022-12-06 20:49:57 +0100

    listings function upstream

--- index.mjs
+++ index.mjs
@@ -2,3 +2,4 @@ import * as auth from "/src/js/auth/index";
 import * as profiles from "/src/js/profiles";
 import * as api from "/src/js/api/index";
 import * as listeners from "/src/js/listeners/index";
+import * as listings from "/src/js/listings";
