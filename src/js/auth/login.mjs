9660dc8f74c1d023d311912b96a32df90886b27a
Liridona <91525128+barlaek@users.noreply.github.com>
2022-12-10 14:40:08 +0100

    cleaning up login function

--- src/js/auth/login.mjs
+++ src/js/auth/login.mjs
@@ -7,30 +7,6 @@ import { saveToken } from "../storage/index.mjs";
 //   password: "assjulebrus69",
 // };
 
-// export const loginForm = document.getElementById("loginForm");
-
-// export const loginForm = document
-//   .getElementById("loginForm")
-//   .addEventListener("submit", (event) => {
-//     event.preventDefault();
-//     const form = event.target;
-//     const user = {
-//       user: form.loginEmail.value,
-//       password: form.loginPassword.value,
-//     };
-//     console.log(user);
-//     async function loginUser(user) {
-//       const postData = {
-//         method: "POST",
-//         headers: headers("application/json"),
-//         body: JSON.stringify(user),
-//       };
-//       const response = await fetch(`${API_LOGIN}`, postData);
-//       console.log(response);
-//     }
-//     loginUser(user);
-//   });
-
 export async function loginUser(user) {
   const postData = {
     method: "POST",
