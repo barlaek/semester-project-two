9e5546861d551df6abe284a4dc4e3fb066435d17
Liridona <91525128+barlaek@users.noreply.github.com>
2022-12-10 16:15:44 +0100

    cleanup login at listeners

--- src/js/listeners/login.mjs
+++ src/js/listeners/login.mjs
@@ -5,10 +5,6 @@ export const form = document
   .addEventListener("submit", (event) => {
     event.preventDefault;
     const form = event.target;
-    // const user = {
-    //   email: form.loginEmail.value,
-    //   password: form.loginPassword.value,
-    // };
     const data = new FormData(form);
     const email = data.get("email");
     const password = data.get("password");
