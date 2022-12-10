a27a543bca29e5b62f5430cc09e0de9a9a2f4dc3
Liridona <91525128+barlaek@users.noreply.github.com>
2022-12-10 16:22:26 +0100

    logout debug

--- src/js/listeners/logout.mjs
+++ src/js/listeners/logout.mjs
@@ -2,7 +2,7 @@ import { logoutUser } from "../auth/logout.mjs";
 
 export const logout = document
   .getElementById("logoutBtn")
-  .addEventListener("submit", (event) => {
+  .addEventListener("click", (event) => {
     event.preventDefault();
     logoutUser();
     window.location.href = "/";
