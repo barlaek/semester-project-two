224eba8b9d0dbe8dfe500071b251f5006265b2c2
Liridona <91525128+barlaek@users.noreply.github.com>
2022-12-10 16:35:27 +0100

    login debugging

--- src/js/listeners/login.mjs
+++ src/js/listeners/login.mjs
@@ -9,5 +9,7 @@ export const form = document
     const email = data.get("email");
     const password = data.get("password");
     const { name } = loginUser(email, password);
-    window.location.replace = `/index.html?view=profile&name=${name}`;
+    window.location.replace = `/index.html/?view=profile&name=${name}`;
   });
+
+  
