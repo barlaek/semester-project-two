89748eb87725a7eb407e2fa183729169beb3c4ef
Liridona <91525128+barlaek@users.noreply.github.com>
2022-12-11 12:03:49 +0100

    router directory and imports added

--- src/js/profiles/profiles.mjs
+++ src/js/profiles/profiles.mjs
@@ -2,14 +2,14 @@ import { API_PROFILES } from "../api/apiPaths.mjs";
 import { getSearchParams } from "../api/searchParams.mjs";
 import { headers } from "../auth/headers.mjs";
 
-// export async function getProfiles() {
-//   const response = await fetch(`${API_PROFILES}`, { headers: headers() });
-//   console.log(response);
-//   const json = await response.json();
-//   console.log(json);
-// }
+export async function getProfiles() {
+  const response = await fetch(`${API_PROFILES}`, { headers: headers() });
+  console.log(response);
+  const json = await response.json();
+  console.log(json);
+}
 
-// getProfiles();
+getProfiles();
 
 // export async function getProfile(name) {
 //   const response = await fetch(`${profileUrl}?_listings=true`, {
---
+++ src/js/router/index.mjs
@@ -0,0 +1,6 @@
+import { getListings } from "../listings/listings.mjs";
+import { getProfiles } from "../profiles/profiles.mjs";
+import { loadToken, saveToken } from "../storage/storage.mjs";
+import { getSearchParams } from "../api/searchParams.mjs";
+import { isLoggedIn } from "../auth/state.mjs";
+
