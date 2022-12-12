521e6e57fe7d45ada79b03c0a8d5a457f5d49a87
Liridona <91525128+barlaek@users.noreply.github.com>
2022-12-12 19:15:02 +0100

    editing paths

--- src/js/ui/listing.mjs
+++ src/js/ui/listing.mjs
@@ -1,18 +1,18 @@
 import { isLoggedIn, profile } from "../auth/state.mjs";
 import { getListing } from "../listings/listings.mjs";
 
-export const listingPage = async (listingId) => {
-  if (!isLoggedIn()) {
-    location.href = "/";
-  } else {
-    profile();
-  }
+// export const listingPage = async (listingId) => {
+//   if (!isLoggedIn()) {
+//     location.href = "/";
+//   } else {
+//     profile();
+//   }
 
-  if (listingId) {
-    const listing = await getListing(listingId);
-    const owner = listing.seller.name === me.name;
-    console.log(listing);
-  }
-};
+//   if (listingId) {
+//     const listing = await getListing(listingId);
+//     const owner = listing.seller.name === me.name;
+//     console.log(listing);
+//   }
+// };
 
 // listingPage();
