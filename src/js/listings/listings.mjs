db0b2ea2a837eebf89537dc46b3fc2ea2c6f8d45
Liridona <91525128+barlaek@users.noreply.github.com>
2022-12-16 17:46:10 +0100

    prepping for listing id page

--- src/js/listings/listings.mjs
+++ src/js/listings/listings.mjs
@@ -58,18 +58,16 @@ function filterByPopularity(listings) {
 
 // getListings();
 
-// export async function getListing(id) {
-//   const getData = {
-//     method: "GET",
-//     headers: headers("application/json"),
-//     body: JSON.stringify(),
-//   };
-//   const response = await fetch(
-//     `${API_LISTINGS}/${id}?_seller=true&_bids=true`,
-//     getData
-//   );
-//   const json = await response.json();
-//   console.log(json);
-// }
-
-// getListing("9af14798-d6b7-4c06-8fc8-09c1fa1c09bb");
+export async function getListing(id) {
+  const getData = {
+    method: "GET",
+    headers: headers("application/json"),
+    body: JSON.stringify(),
+  };
+  const response = await fetch(
+    `${API_LISTINGS}/${id}?_seller=true&_bids=true`,
+    getData
+  );
+  const json = await response.json();
+  console.log(json);
+}
--- src/js/ui/header.mjs
+++ src/js/ui/header.mjs
@@ -51,7 +51,7 @@ export async function header() {
   //     createListing(newListing);
   //   });
 
-  getListings();
+  // getListings();
 
   // let listings = [];
 
