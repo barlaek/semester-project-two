dc0abb4f23c0f3472b85b1274e64a234e4824333
Liridona <91525128+barlaek@users.noreply.github.com>
2022-12-16 16:29:30 +0100

    debugging

--- src/js/ui/listings.mjs
+++ src/js/ui/listings.mjs
@@ -7,7 +7,6 @@ export function viewListings(listings) {
       return (listingsContainer.innerHTML += `<a href="/html/listing.html?id=${listing.id}">
         <img src="${listing.media}"/>
         <p>${listing.title}</p>
-        <p>Current bid: ${listing.bids}</p>
         <p>Ends at ${listing.endsAt}</p>
         </a>`);
     });
