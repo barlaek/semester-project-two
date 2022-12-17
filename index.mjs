94e288752d472464b002acff8d035205e274d605
Liridona <91525128+barlaek@users.noreply.github.com>
2022-12-17 23:18:28 +0100

    functioning app

--- index.mjs
+++ index.mjs
@@ -1,38 +1,5 @@
-// import * as api from "/src/js/api/index";
-// import * as auth from "/src/js/auth/index";
-// import * as profiles from "/src/js/profiles";
-// import * as listings from "/src/js/listings";
-// import * as listeners from "/src/js/listeners/index";
-
-// import { listing } from "./src/js/listings/listing.mjs";
 import { header, indexList } from "./src/js/ui/header.mjs";
 
-// import { createListing } from "./src/js/listings/createListing.mjs";
-
-// import router from "./src/js/router/index.mjs";
-
-// // import router from "./src/js/router/index.mjs";
-
-// // router();
-
-// // import { newUser } from "./src/js/listeners/register.mjs";
-
-// // import { form } from "./src/js/listeners/login.mjs";
-
-// /**ostepop ostepop@noroff.no ostepop69 */
-
-// import * as header from "./src/js/ui/header.mjs";
-
-// import { logout } from "./src/js/listeners/logout.mjs";
-
-// import { getListings } from "./src/js/listings/listings.mjs";
-
-// // import { search } from "./src/js/listings/search.mjs";
-// // import { listingPage } from "./src/js/ui/listing.mjs";
-
-// router();
-// import * as index from "./src/js/listings/index.mjs";
-
 header();
-// indexList();
-// listing()
+indexList();
+listing();
