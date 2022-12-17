9866711429f6070208c4679caeefb1b72b6c51d0
Liridona <91525128+barlaek@users.noreply.github.com>
2022-12-17 14:06:45 +0100

    trying to debug bid

--- src/js/listeners/bid.mjs
+++ src/js/listeners/bid.mjs
@@ -1,23 +1,42 @@
-import { API_LISTINGS } from "../api/apiPaths.mjs";
 import { placeBid } from "../listings/bid.mjs";
+import { getSearchParams } from "../api/searchParams.mjs";
+
+async function bidBtn(event) {
+  event.preventDefault();
+  const form = event.target;
+  const data = new FormData(form);
+  const body = data.get("body");
+  const listingId = form.dataset.listingId;
+  const bid = getSearchParams().bid;
+  await placeBid(listingId, body, bid);
+  console.log(body, listingId, bid);
+  location.reload();
+}
+
+const bidForm = document
+  .getElementById("bidForm")
+  .addEventListener("submit", (event) => {
+    event.preventDefault();
+    bidBtn();
+  });
 
 // const queryString = document.location.search;
 // const params = new URLSearchParams(queryString);
 // const id = params.get("id");
 // const url = `${API_LISTINGS}/${id}/bids`;
 
-export const bid = document
-  .getElementById("bidForm")
-  .addEventListener("submit", (event) => {
-    event.preventDefault();
-    const form = event.target;
-    const bid = {
-      amount: parseInt(form.placeBid.value),
-    };
-    placeBid(bid);
-    console.log(bid);
-    // window.location.replace(`${url}=${bid}?_seller=true&_bids=true`);
-});
+// export const bid = document
+//   .getElementById("bidForm")
+//   .addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     const form = event.target;
+//     const bid = {
+//       amount: parseInt(form.placeBid.value),
+//     };
+//     placeBid(bid);
+//     console.log(bid);
+//     // window.location.replace(`${url}=${bid}?_seller=true&_bids=true`);
+//   });
 
 // const bidBtn = document
 //   .getElementById("bid")
--- src/js/listings/bid.mjs
+++ src/js/listings/bid.mjs
@@ -1,19 +1,19 @@
 import { API_LISTINGS } from "../api/apiPaths.mjs";
 import { headers } from "../auth/headers.mjs";
 
-const queryString = document.location.search;
-const params = new URLSearchParams(queryString);
-const id = params.get("id");
-const url = `${API_LISTINGS}/${id}/bids`;
+// const queryString = document.location.search;
+// const params = new URLSearchParams(queryString);
+// const id = params.get("id");
+// const url = `${API_LISTINGS}/${id}/bids`;
 
-export async function placeBid(bid) {
+export async function placeBid(listingID, body, bid) {
   const postData = {
     method: "POST",
     headers: headers("application/json"),
-    method: JSON.stringify(bid),
+    method: JSON.stringify({ body, bid }),
   };
 
-  const response = await fetch(`${url}`, postData);
+  const response = await fetch(`${API_LISTINGS}/${listingID}/bids`, postData);
   console.log(response);
   const json = await response.json();
   console.log(json);
