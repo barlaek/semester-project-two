b8f3f8e682b9c141bb7dd64d45e2506bb431fd23
Liridona <91525128+barlaek@users.noreply.github.com>
2022-12-17 16:36:01 +0100

    profile elements

--- src/js/ui/profile.mjs
+++ src/js/ui/profile.mjs
@@ -1,9 +1,13 @@
-export async function viewProfile(profile) {
-    const profileContainer = document.getElementById("profileContainer")
-    profileContainer.innerHTML = ""
-    if(profile) {
-        profileContainer.innerHTML = `
-        <h2>${profile.name}</h2>
-        `
-    }
+export function viewProfile(profile) {
+  const profileContainer = document.getElementById("profileContainer");
+  profileContainer.innerHTML = "";
+  if (profile) {
+    const profileImg = document
+      .getElementById("profileImg")
+      .append(profile.avatar);
+
+    const profileName = document
+      .getElementById("profileName")
+      .append(profile.name);
+  }
 }
