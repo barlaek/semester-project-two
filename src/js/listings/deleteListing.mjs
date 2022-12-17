acf95ab6a3558c986dcc889bd7c56b45867f2c66
Liridona <91525128+barlaek@users.noreply.github.com>
2022-12-17 22:49:31 +0100

    cleanup

---
+++ src/js/listings/deleteListing.mjs
@@ -0,0 +1,26 @@
+import { API_LISTINGS } from "../api/apiPaths.mjs";
+import { headers } from "../auth/headers.mjs";
+import { loadToken } from "../storage/storage.mjs";
+
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const id = params.get("id");
+const url = `${API_LISTINGS}/${id}`;
+
+export async function deleteListing(id) {
+  try {
+    const deleteData = {
+      method: "DELETE",
+      headers: headers("application/json"),
+      body: JSON.stringify(id),
+    };
+
+    const response = await fetch(`${url}`, deleteData);
+    const profile = loadToken("profile");
+    if (response) {
+      location.href = `/html/profile.html?name=${profile.name}`;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
