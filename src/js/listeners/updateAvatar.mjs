53ab6774180291ad4b1c2b1c6ca39f57f704832e
Liridona <91525128+barlaek@users.noreply.github.com>
2022-12-17 20:18:28 +0100

    update avatar

--- src/js/listeners/updateAvatar.mjs
+++ src/js/listeners/updateAvatar.mjs
@@ -1,19 +1,23 @@
 import { API_PROFILES } from "../api/apiPaths.mjs";
 import { headers } from "../auth/headers.mjs";
+import { loadToken } from "../storage/storage.mjs";
 
-const queryString = document.location.search;
-const params = new URLSearchParams(queryString);
-const name = params.get("name");
-const url = `${API_PROFILES}/${name}/media`;
+// const queryString = document.location.search;
+// const params = new URLSearchParams(queryString);
+// const name = params.get("name");
+// const url = `${API_PROFILES}/${name}/media`;
+
+const profile = loadToken("profile");
+const name = profile.name;
 
 async function updateAvatar(mediaURL) {
   const putData = {
     method: "PUT",
     headers: headers("application/json"),
-    body: JSON.stringify(url),
+    body: JSON.stringify(mediaURL),
   };
 
-  const response = await fetch(`${url}`, putData);
+  const response = await fetch(`${API_PROFILES}/${name}/media`, putData);
   console.log(response);
   const json = await response.json();
   console.log(json);
@@ -22,10 +26,12 @@ async function updateAvatar(mediaURL) {
 const updateForm = document
   .getElementById("updateForm")
   .addEventListener("submit", (event) => {
-    event.defaultPrevented();
+    event.preventDefault();
     const form = event.target;
     const media = {
       avatar: form.updateURL.value,
     };
     updateAvatar(media);
+    console.log(media);
+    location.href = `/html/profile.html?name=${name}`;
   });
