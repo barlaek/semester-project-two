f027aab284496ea125b76e6676abbb98ceac16d2
Liridona <91525128+barlaek@users.noreply.github.com>
2022-12-18 11:01:06 +0100

    cleanup at update

--- src/js/listings/updateListing.mjs
+++ src/js/listings/updateListing.mjs
@@ -6,17 +6,18 @@ const params = new URLSearchParams(queryString);
 const id = params.get("id");
 
 export async function updateListing(input) {
-  const upData = {
-    method: "PUT",
-    headers: headers("application/json"),
-    body: JSON.stringify(input),
-  };
+  try {
+    const upData = {
+      method: "PUT",
+      headers: headers("application/json"),
+      body: JSON.stringify(input),
+    };
 
-  const response = await fetch(`${API_LISTINGS}/${id}`, upData);
-  console.log(response);
-  const json = await response.json();
-  console.log(json);
-  if (response.ok) {
-    location.reload();
+    const response = await fetch(`${API_LISTINGS}/${id}`, upData);
+    if (response.ok) {
+      location.reload();
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
