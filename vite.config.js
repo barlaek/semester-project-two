d9fe41ed58ecc13c17bf893972f38cdb31555f03
Liridona <91525128+barlaek@users.noreply.github.com>
2022-12-18 16:06:13 +0100

    debugging build command

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-export default {
-  server: {
-    port: 5173,
-    hot: true,
-    host: "127.0.0.1",
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "src/html/createListing.html"),
+        nested: resolve(__dirname, "src/html/listing.html"),
+        nested: resolve(__dirname, "src/html/login.html"),
+        nested: resolve(__dirname, "src/html/profile.html"),
+        nested: resolve(__dirname, "src/html/register.html"),
+        nested: resolve(__dirname, "src/html/updateAvatar.html"),
+        nested: resolve(__dirname, "src/html/updateListing.html"),
+      },
+    },
   },
-};
+});
+
+// export default {
+//   server: {
+//     port: 5173,
+//     hot: true,
+//     host: "127.0.0.1",
+//   },
+// };
