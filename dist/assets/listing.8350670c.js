e4809a2101ea6f9ac09a5640855759bc8b80ac9b
Liridona <91525128+barlaek@users.noreply.github.com>
2023-06-03 18:10:02 +0200

    tailwind css is working

---
+++ dist/assets/listing.8350670c.js
@@ -0,0 +1,9 @@
+import{A as o,h as s}from"./headers.e04ea350.js";function r(e){const t=document.getElementById("listingContainer");t.innerHTML="",e&&(t.innerHTML=`<div class="bg-red-400">
+            <img src="${e.media}" alt="${e.description}">
+            <div>
+                <h2>${e.title}</h2>
+                <p>${e.description}</p>
+                <h4>current bids:</h4>
+                ${e.bids.map(n=>`<p>bidder: ${n.bidderName} : amount: ${n.amount}</p>`)}
+            </div>
+        </div>`)}const a=document.location.search,i=new URLSearchParams(a),c=i.get("id"),d=new URL(`${o}/${c}`);async function m(){try{const e={method:"GET",headers:s("application/json"),body:JSON.stringify()},t=await fetch(`${d}?_seller=true&_bids=true`,e),n=await t.json();t.ok?r(n):alert("oops! something went wrong!")}catch(e){console.log(e)}}m();const l=document.location.search,p=new URLSearchParams(l),u=p.get("id"),h=`${o}/${u}/bids`;async function g(e){try{const t={method:"POST",headers:s("application/json"),body:JSON.stringify(e)};(await fetch(`${h}`,t)).ok?location.reload():alert("oops! something went wrong")}catch(t){console.log(t)}}document.getElementById("bidForm").addEventListener("submit",e=>{e.preventDefault();const t=e.target,n={amount:parseInt(t.placeBid.value)};g(n)});
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["/dist/*.html", "./dist/**/*.{html,js}"],
   theme: {
     extend: {},
   },
